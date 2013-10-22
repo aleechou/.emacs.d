a2ba1dfe76dcdec5247cb2378f8ebfbb8b68c0fb
alee chou <zq@alee2002.com>
2013-10-22 16:19:20 +0800

    first commit,  install js2-mode/coffee-mode ...

---
+++ el-get/.status.el
@@ -0,0 +1,37 @@
+((coffee-mode status "installed" recipe
+	      (:name coffee-mode :website "http://ozmm.org/posts/coffee_mode.html" :description "Emacs Major Mode for CoffeeScript" :type github :pkgname "defunkt/coffee-mode" :features coffee-mode :post-init
+		     (progn
+		       (add-to-list 'auto-mode-alist
+				    '("\\.coffee$" . coffee-mode))
+		       (add-to-list 'auto-mode-alist
+				    '("Cakefile" . coffee-mode))
+		       (setq coffee-js-mode 'javascript-mode))))
+ (css-mode status "installed" recipe
+	   (:name css-mode :description "Minor mode for CSS" :features css-mode :type elpa))
+ (el-get status "installed" recipe
+	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :features el-get :info "." :load "el-get.el"))
+ (js2-mode status "installed" recipe
+	   (:name js2-mode :website "https://github.com/mooz/js2-mode#readme" :description "An improved JavaScript editing mode" :type github :pkgname "mooz/js2-mode" :prepare
+		  (autoload 'js2-mode "js2-mode" nil t)))
+ (package status "installed" recipe
+	  (:name package :description "ELPA implementation (\"package.el\") from Emacs 24" :builtin 24 :type http :url "http://repo.or.cz/w/emacs.git/blob_plain/1a0a666f941c99882093d7bd08ced15033bc3f0c:/lisp/emacs-lisp/package.el" :shallow nil :features package :post-init
+		 (progn
+		   (setq package-user-dir
+			 (expand-file-name
+			  (convert-standard-filename
+			   (concat
+			    (file-name-as-directory default-directory)
+			    "elpa")))
+			 package-directory-list
+			 (list
+			  (file-name-as-directory package-user-dir)
+			  "/usr/share/emacs/site-lisp/elpa/"))
+		   (make-directory package-user-dir t)
+		   (unless
+		       (boundp 'package-subdirectory-regexp)
+		     (defconst package-subdirectory-regexp "^\\([^.].*\\)-\\([0-9]+\\(?:[.][0-9]+\\)*\\)$" "Regular expression matching the name of\n a package subdirectory. The first subexpression is the package\n name. The second subexpression is the version string."))
+		   (setq package-archives
+			 '(("ELPA" . "http://tromey.com/elpa/")
+			   ("gnu" . "http://elpa.gnu.org/packages/")
+			   ("marmalade" . "http://marmalade-repo.org/packages/")
+			   ("SC" . "http://joseito.republika.pl/sunrise-commander/")))))))
