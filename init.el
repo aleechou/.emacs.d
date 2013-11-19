7f7d456317aa73282c92bc1eb7c96137de5723b1
alee chou <zq@alee2002.com>
2013-11-19 23:35:24 +0800

    copy/cut whole line

--- init.el
+++ init.el
@@ -18,6 +18,31 @@
       (color-theme-tomorrow-night)))
 
 
+
+;; copy region or whole line
+(global-set-key "\M-w"
+(lambda ()
+  (interactive)
+  (if mark-active
+      (kill-ring-save (region-beginning)
+      (region-end))
+    (progn
+     (kill-ring-save (line-beginning-position)
+     (line-end-position))
+     (message "copied line")))))
+;; kill region or whole line
+(global-set-key "\C-w"
+(lambda ()
+  (interactive)
+  (if mark-active
+      (kill-region (region-beginning)
+   (region-end))
+    (progn
+     (kill-region (line-beginning-position)
+  (line-end-position))
+     (message "killed line")))))
+
+
 ;; ----------------------------- 
 ;; 防止鼠标滚动太快
 (setq mouse-wheel-scroll-amount '(2 ((shift) . 2)((control)))
